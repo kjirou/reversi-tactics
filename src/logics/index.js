3ab5fc7acd1a95d0e5eb0077f2f70c5d70abdef7
kjirou <sorenariblog@gmail.com>
2016-02-16 13:39:53 +0900

    Add a logics module

---
+++ src/logics/index.js
@@ -0,0 +1,66 @@
+import { SCENE_IDS } from '../consts';
+
+
+/*
+ * Resolve the logic execution by passing the dependencies and args
+ *
+ * @param {Function} logic - One of business logics
+ * @param {object} dependencies - { appModel }
+ * @param {Array} logicArgs
+ */
+export const _resolveLogic = (logic, dependencies, logicArgs = []) => {
+  let result;
+
+  try {
+    result = logic(dependencies, ...logicArgs);
+  } catch (err) {
+    result = err;
+  }
+
+  // Convert to Promise-based API forcibly
+  let promisifiedResult;
+  if (result instanceof Promise) {
+    promisifiedResult = result;
+  } else if (result instanceof Error) {
+    promisifiedResult = Promise.reject(result);
+  } else {
+    promisifiedResult = Promise.resolve(result);
+  }
+
+  return promisifiedResult;
+};
+
+export const bindLogics = (logics, appModel) => {
+  const dependencies = {
+    model: appModel,
+  };
+
+  const boundLogics = {};
+  Object.keys(logics).forEach(logicName => {
+    const logic = logics[logicName];
+    boundLogics[logicName] = (...logicArgs) => _resolveLogic(logic, dependencies, logicArgs);
+  });
+
+  return boundLogics;
+};
+
+/*
+ * Business logics
+ *
+ * Generally each logic becomes one-to-one with the UI, but not necessarily.
+ * For example, sometimes one of the logic is used for plural UIs.
+ */
+export const logics = {
+
+  // TODO: private properties are used
+  touchSquare({ model }, position) {
+    if (!model._game) {
+      return;
+    }
+    model._lastProceedingResult = model._game.proceed(position);
+  },
+
+  touchStart({ model }) {
+    model._sceneId = SCENE_IDS.GAME;
+  },
+};
